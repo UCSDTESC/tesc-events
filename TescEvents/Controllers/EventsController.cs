319beb8dc184208c643b66cf49ec832ea1166fbb
Shane Kim <shanekim28@gmail.com>
2022-10-12 11:04:57 -0700

    Create event route

--- TescEvents/Controllers/EventsController.cs
+++ TescEvents/Controllers/EventsController.cs
@@ -26,17 +26,22 @@ public class EventsController : ControllerBase {
         }
 
         if (!DateTime.TryParse(end, out var endFilter)) {
-            endFilter = DateTime.MinValue;
+            endFilter = DateTime.MaxValue;
         }
 
-        return Ok(eventRepository.FindByCondition(e => e.Start >= startFilter.ToUniversalTime() && e.End <= endFilter
-                                                      .ToUniversalTime()));
+        return Ok(eventRepository.FindByCondition(e => e.Start >= startFilter.ToUniversalTime() 
+                                                       && e.End <= endFilter.ToUniversalTime()));
     }
 
-    [Authorize]
     [HttpPost(Name = nameof(CreateEvent))]
-    public Task<IActionResult> CreateEvent(EventCreateRequestDTO e) {
+    public async Task<IActionResult> CreateEvent(EventCreateRequestDTO e) {
+        var eventEntity = mapper.Map<Event>(e);
         
-        throw new NotImplementedException();
+        // TODO: Abstract into service transaction and async call
+        eventRepository.Create(eventEntity);
+        eventRepository.Save();
+
+        var eventResponse = mapper.Map<EventPublicResponseDTO>(eventEntity);
+        return CreatedAtRoute(nameof(CreateEvent), new { Id = eventResponse.Id }, eventResponse);
     }
 }
--- TescEvents/DTOs/Events/EventPublicResponseDTO.cs
+++ TescEvents/DTOs/Events/EventPublicResponseDTO.cs
@@ -1,6 +1,7 @@
 namespace TescEvents.DTOs.Events;
 
 public class EventPublicResponseDTO {
+    public string Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public IFormFile? Thumbnail { get; set; }
