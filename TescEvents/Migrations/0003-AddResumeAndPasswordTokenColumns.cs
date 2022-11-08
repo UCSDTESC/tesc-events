24ab18e44ca7065d3ce048f6affe1c7b327df079
Shane Kim <shanekim28@gmail.com>
2022-11-07 22:23:04 -0800

    Add migration

---
+++ TescEvents/Migrations/0003-AddResumeAndPasswordTokenColumns.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TescEvents.Migrations
+{
+    public partial class AddResumeAndPasswordTokenColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResetToken",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeUrl",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeUrl",
+                table: "Users");
+        }
+    }
+}
