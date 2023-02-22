821e5395946a165b6a0fbb8c18777992bc41d0e9
Nikhil Dange <nsdange@ucsd.edu>
2023-02-21 17:22:53 -0800

    changes

--- TescEvents/Services/EventService.cs
+++ TescEvents/Services/EventService.cs
@@ -9,12 +9,16 @@ public class EventService : IEventService {
     public EventService(RepositoryContext context) : base() { }
 
     public IQueryable<Event> GetFutureEvents() {
-        throw new NotImplementedException();
-        
+        //throw new NotImplementedException();
+        DateTime timeNow = DateTime.Now;
+        var futureEvents = RepositoryContext.Events.Where(u => u.Start >= timeNow);
+        return futureEvents;
     }
 
     public Event? GetEventDetails(Guid eventId) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        var event = RepositoryContext.Events!.FirstOrDefault(e => e.Id == eventId);
+        return event;
     }
     
 }
